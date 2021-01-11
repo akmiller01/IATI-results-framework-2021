27c64721b54d6b0b8a0081100b784b8e8d1a5a67
Alex Miller <alex.k.miller@gmail.com>
2021-01-11 11:14:19 -0500

    Categorize

--- calculate_bins.R
+++ calculate_bins.R
@@ -142,4 +142,26 @@ dat_tab = subset(dat_tab,ex.rate>0 & type %in% c(
 dat_tab$value_usd = dat_tab$value / dat_tab$ex.rate
 publisher_tab = dat_tab[,.(value_usd=sum(value_usd,na.rm=T)),by=.(year,reporting_org_ref)]
 publisher_tab = publisher_tab[order(publisher_tab$reporting_org_ref,-publisher_tab$year),]
+
+publisher_tab$category = "<=1M"
+publisher_tab$category[which(publisher_tab$value_usd<=1000000)] = "<=1M"
+publisher_tab$category[which(publisher_tab$value_usd>1000000 & publisher_tab$value_usd<=10000000)] = "> 1M & <= 10M"
+publisher_tab$category[which(publisher_tab$value_usd>10000000 & publisher_tab$value_usd<=100000000)] = "> 10M & <= 100M"
+publisher_tab$category[which(publisher_tab$value_usd>100000000 & publisher_tab$value_usd<=1000000000)] = "> 100M & <= 1B"
+publisher_tab$category[which(publisher_tab$value_usd>1000000000)] = "> 1B"
+
 fwrite(publisher_tab,"IATI_publishers_by_spend_10012021.csv")
+
+publisher_tab$category = factor(
+  publisher_tab$category,
+  levels=c(
+    "<=1M",
+    "> 1M & <= 10M",
+    "> 10M & <= 100M",
+    "> 100M & <= 1B",
+    "> 1B"
+  )
+)
+
+publisher_tab_2020 = subset(publisher_tab,year==2020)
+ggplot(publisher_tab_2020,aes(x=category)) + geom_bar()
