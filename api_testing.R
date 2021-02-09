d547e0e6cb63c419fd49aa6db4ddba94a4ae69fc
Alex Miller <alex.k.miller@gmail.com>
2021-02-09 08:57:58 -0500

    Simple merge

--- api_testing.R
+++ api_testing.R
@@ -7,61 +7,22 @@ wd = "~/git/IATI-results-framework-2021"
 
 setwd(wd)
 
-# Download duplicate reporting_org_ref in data table, split
-# table_url = "http://dashboard.iatistandard.org/reporting_orgs.html"
-# table_url_src = content(GET(table_url))
-# table_doc = htmlParse(table_url_src)
-# pub_tab = readHTMLTable(table_doc)[[1]]
-# names(pub_tab) = make.names(names(pub_tab))
-# pub_tab$Reporting.Org.on.Registry = as.character(pub_tab$Reporting.Org.on.Registry)
-# pub_tab$Reporting.Org.on.Registry[which(pub_tab$Reporting.Org.on.Registry=="NL-KVK-34200988 (deprecated)")] = "NL-KVK-34200988"
-# pub_tab$Reporting.Org.on.Registry[which(pub_tab$Reporting.Org.on.Registry=="US-EIN 81-2339233")] = "US-EIN-81-2339233"
-# pub_tab = pub_tab[,c("Reporting.Org.on.Registry","Reporting.Orgs.in.Data")]
-# pub_tab_split = cSplit(pub_tab,"Reporting.Orgs.in.Data",sep=" ")
-# new_names = names(pub_tab_split)[which(names(pub_tab_split)!="Reporting.Org.on.Registry")]
-# pub_tab_split_long = reshape(pub_tab_split, varying=new_names, direction="long", sep="_")
-# pub_tab_split_long[,c("time","id")] = NULL
-# pub_tab_split_long=subset(pub_tab_split_long, !is.na(Reporting.Orgs.in.Data) & Reporting.Orgs.in.Data!="null")
-# fwrite(pub_tab_split_long,"publisher_data_names.csv")
-pub_tab_split_long = fread("publisher_data_names.csv")
 
 # Load publisher table from registry, use to create full publisher reporting_org_ref table
 org_ids = fread("iati-publishers.csv")
 org_ids = unique(org_ids[,c("IATI Organisation Identifier","publisher")])
-setnames(org_ids,"IATI Organisation Identifier","Reporting.Org.on.Registry")
-single_orgs = setdiff(org_ids$Reporting.Org.on.Registry,pub_tab_split_long$Reporting.Org.on.Registry)
-single_org_df = data.frame(Reporting.Org.on.Registry=single_orgs, Reporting.Orgs.in.Data=single_orgs)
-pub_tab_split_long = rbind(pub_tab_split_long, single_org_df)
-setnames(pub_tab_split_long, "Reporting.Orgs.in.Data", "reporting_org_ref")
-
-# Fix duplicate publishers claiming same reporting_org_ref
-pub_tab_split_long = subset(
-  pub_tab_split_long,
-  !(Reporting.Org.on.Registry=="BJ-IFU-6201408024805" & reporting_org_ref=="BJ-IFU-4201710012551") &
-    !(Reporting.Org.on.Registry=="US-EIN-23-1352010" & reporting_org_ref=="NL-KVK-41149831") &
-    !(Reporting.Org.on.Registry=="NL-KVK-27327087" & reporting_org_ref=="NL-KVK-41150939") &
-    !(Reporting.Org.on.Registry=="NL-KVK-70292361" & reporting_org_ref=="NL-KVK-53744993")
-)
 
 # Load total spend, recalculate categories, merge publishers
+setnames(org_ids,"IATI Organisation Identifier","reporting_org_ref")
 org_categories = fread("output/IATI_publishers_by_spend_10012021.csv")
-org_categories = merge(org_categories, pub_tab_split_long, by="reporting_org_ref", all.x=T)
-org_categories$Reporting.Org.on.Registry = as.character(org_categories$Reporting.Org.on.Registry)
-org_categories$reporting_org_ref = as.character(org_categories$reporting_org_ref)
-org_categories$Reporting.Org.on.Registry[which(is.na(org_categories$Reporting.Org.on.Registry))] = org_categories$reporting_org_ref[which(is.na(org_categories$Reporting.Org.on.Registry))]
-org_categories = org_categories[,.(value_usd=sum(value_usd,na.rm=T)),by=.(Reporting.Org.on.Registry, year)]
-org_categories$category = "<=1M"
-org_categories$category[which(org_categories$value_usd<=1000000)] = "<=1M"
-org_categories$category[which(org_categories$value_usd>1000000 & org_categories$value_usd<=10000000)] = "> 1M & <= 10M"
-org_categories$category[which(org_categories$value_usd>10000000 & org_categories$value_usd<=100000000)] = "> 10M & <= 100M"
-org_categories$category[which(org_categories$value_usd>100000000 & org_categories$value_usd<=1000000000)] = "> 100M & <= 1B"
-org_categories$category[which(org_categories$value_usd>1000000000)] = "> 1B"
+org_categories = merge(org_categories, org_ids, by="reporting_org_ref", all=T)
+org_categories$category[which(is.na(org_categories$category))] = "<=1M"
 
 org_categories = subset(org_categories,year %in% c(2018,2019,2020))
-org_cat_max = data.table(org_categories)[,.(category=category[which.max(.SD$value_usd)],year=year[which.max(.SD$value_usd)]),by=.(Reporting.Org.on.Registry)]
-org_cat_max = merge(org_cat_max, org_ids, by="Reporting.Org.on.Registry",all=T)
+org_cat_max = data.table(org_categories)[,.(category=category[which.max(.SD$value_usd)],year=year[which.max(.SD$value_usd)]),by=.(reporting_org_ref)]
+org_cat_max = merge(org_cat_max, org_ids, by="reporting_org_ref",all=T)
 org_cat_max$category[which(is.na(org_cat_max$category))] = "<=1M"
-org_cat_max$Reporting.Org.on.Registry = NULL
+org_cat_max$reporting_org_ref = NULL
 setnames(org_cat_max,"year","category_year")
 
 # test_url = "https://iativalidator.iatistandard.org/api/v1/stats?date=2021-01-11"
@@ -100,9 +61,11 @@ close(pb)
 all.messages = rbindlist(meta.mess.list)
 all.messages = all.messages[order(all.messages$publisher),]
 all.messages = merge(all.messages, org_cat_max, by="publisher", all.x=T)
+all.messages$category[which(is.na(all.messages$category))] = "<=1M"
 all.summaries = rbindlist(meta.sum.list)
 setnames(all.summaries,"count","value")
 all.summaries.wide = dcast(all.summaries,publisher~name)
 all.summaries.wide = merge(all.summaries.wide, org_cat_max, by="publisher", all.x=T)
+all.summaries.wide$category[which(is.na(all.summaries.wide$category))] = "<=1M"
 fwrite(all.messages,"output/all_messages.csv")
 fwrite(all.summaries.wide,"output/all_summaries.csv")
